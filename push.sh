4ef9d8ef76e9edbd51bcfeb69a1310fb9958f213
Maksim Koltsov <developer@tirinox.ru>
2015-04-09 21:50:45 +0300

    fix

--- push.sh
+++ push.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-git status
-read -p "Press smth to git add -A"
-git add -A
-git status
-read -p "Press smth to git commit with comment" $1
-git commit -m $1
-read -p "Press smth to git push origin master"
-git push origin master
+
+if [ -z "$1" ]; then
+	echo "Usage $0 <comment>";
+else
+	git status
+	read -p "Press smth to git add -A"
+	git add -A
+	git status
+	read -p "Press smth to git commit with comment" $1
+	git commit -m $1
+	echo "\n"
+	read -p "Press smth to git push origin master"
+	git push origin master
+fi
