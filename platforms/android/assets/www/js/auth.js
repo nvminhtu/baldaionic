34f28620eb7dd0ec8efb379749a7d311edb904e1
Maksim Koltsov <developer@tirinox.ru>
2015-04-10 07:01:23 +0300

    build for android

--- platforms/android/assets/www/js/auth.js
+++ platforms/android/assets/www/js/auth.js
@@ -1,2 +1,39 @@
-// auth.js
 
+angular.module('balda').controller('authController', function ($scope, $state, $ionicLoading, socialProvider, server) {
+
+    var m = $scope.model = {
+        name: '',
+        loading: false,
+        isError: true
+    };
+
+
+    $scope.$watch('model.name', function() {
+        var name = m.name.trim();
+        m.isError = name.length < 2 || name.length >= 32;
+        socialProvider.name = name;
+    });
+
+    $scope.gotoGameplay = function() {
+        $state.go('gameplay');
+    };
+
+    $scope.authorizeMe = function() {
+        //if(user.name.length > 0) {
+        //
+        //    $state.go('tabs.newgame');
+        //    //user.name = '';
+        //    //$scope.loading = true;
+        //
+        //    // $ionicLoading.show({
+        //    //     template: 'Please wait while we register your device...'
+        //    // });
+        //}
+        //else
+        //    alert('No name');
+        if(!m.isError) {
+            server.login();
+            $state.go('tabs.newgame');
+        }
+    };
+});
